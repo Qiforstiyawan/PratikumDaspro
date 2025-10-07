c2ebf41726928c5e5191f758e810aa4bbef64b2f
Qiforstiyawan <qiforstiyawan@gmail.com>
2025-10-07 09:24:31 +0700

    Tugas 1

--- jobsheet5/ifElseCetakKRSNoPresensi.java
+++ jobsheet5/ifElseCetakKRSNoPresensi.java
@@ -6,28 +6,15 @@ public class ifElseCetakKRSNoPresensi {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("--- Cetak KRS Siakad ---");
-        System.out.println("Masukkan semester tahun ini: ");
+        System.out.print("Masukkan semester tahun ini: ");
         int semester = sc.nextInt();
 
-        if (semester == 1) {
-            System.out.println("KRS semester 1 ditampilkan");
-        } else if (semester == 2) {
-            System.out.println("KRS semester 2 ditampilkan");
-        } else if (semester == 3) {
-            System.out.println("KRS semester 3 ditampilkan");
-        } else if (semester == 4) {
-            System.out.println("KRS semester 4 ditampilkan");
-        } else if (semester == 5) {
-            System.out.println("KRS semester 5 ditampilkan");
-        } else if (semester == 6) {
-            System.out.println("KRS semester 6 ditampilkan");
-        } else if (semester == 7) {
-            System.out.println("KRS semester 7 ditampilkan");
-        } else if (semester == 8) {
-            System.out.println("KRS semester 8 ditampilkan");
-        } else {
-            System.out.println("Semester tidak valid");
-        }
+        String pesan = (semester >= 1 && semester <= 8)
+        ? "KRS semester " + semester + " ditampilkan"
+        : "Semester tidak valid";
+        System.out.println("== Versi Ternary Operator ==");
+        System.out.println(pesan);
+
     sc.close();
     }
 }
