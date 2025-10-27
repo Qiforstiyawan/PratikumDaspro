4294f47265be2c957b2d56fdf209b75a62dca6b1
Qiforstiyawan <qiforstiyawan@gmail.com>
2025-10-27 18:05:33 +0700

    Modifikasi percobaan 1

--- Jobsheet7/siakadForNoPresensi.java
+++ Jobsheet7/siakadForNoPresensi.java
@@ -8,21 +8,29 @@ public class siakadForNoPresensi {
         Scanner sc = new Scanner(System.in);
 
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
-            if (nilai > tertinggi) {
-                tertinggi = nilai;
-            }
-            if (nilai < terendah) {
-                terendah = nilai;
-            }
-        }
+                if (nilai > tertinggi) {
+                    tertinggi = nilai;
+                }
+                if (nilai < terendah) {
+                    terendah = nilai;
+                }
+                if (nilai <= 60) {
+                    lulus++;
+                } else {
+                    tidakLulus++;
+                }
+        }   
 
         sc.close();
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendadah: " + terendah);
+        System.out.println("Jumlah mahasiswa yg lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yg tidak lulus: " + tidakLulus);
     }
 }
