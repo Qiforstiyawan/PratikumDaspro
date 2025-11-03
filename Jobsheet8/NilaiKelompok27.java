49d3d68ef3aa7249f8c318e925f73d9f29efcf2b
Qiforstiyawan <qiforstiyawan@gmail.com>
2025-11-03 17:58:56 +0700

    Modifikasi Percobaan 4

--- Jobsheet8/NilaiKelompok27.java
+++ Jobsheet8/NilaiKelompok27.java
@@ -3,24 +3,35 @@ import java.util.Scanner;
 public class NilaiKelompok27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int i = 1, j, nilai;
         float totalNilai, rataNilai;
+        float rataTertinggi = 0;
+        int kelompokTertinggi = 0;
 
-        while (i <= 6){
-                System.out.println("kelompok " + i);
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0;
 
-                totalNilai = 0;
-                for (j = 1; j <= 5; j++) {
-                    System.out.print("Nilai dari Kelompok Penilai " + j + ": ");
-                    nilai = sc.nextInt();
-                    totalNilai += nilai;
-                }
+            for (j = 1; j <= 5; j++) {
+                System.out.print("Nilai dari Kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
 
-                rataNilai = totalNilai/5;
-                System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
-                i++;
+            rataNilai = totalNilai / 5;
+            System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
+
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
+            i++;
         }
+
+        System.out.println("\nKelompok dengan nilai rata-rata tertinggi:");
+        System.out.println("Kelompok " + kelompokTertinggi + " dengan rata-rata " + rataTertinggi);
+
         sc.close();
     }
 }
