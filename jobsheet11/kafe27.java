9b1a8327d283d3bb5dd8e3ed2448cc12bd2d5a62
Qiforstiyawan <qiforstiyawan@gmail.com>
2025-12-01 21:17:36 +0700

    Modifikasi Percobaan 2

--- jobsheet11/kafe27.java
+++ jobsheet11/kafe27.java
@@ -1,16 +1,25 @@
 package jobsheet11;
 
 public class kafe27 {
-    public static void Menu(String namaPelanggan, boolean isMember) {
+
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
-            System.out.println("Anda adalah member, dapatkan diskon 10% untuk");
+            System.out.println("Anda adalah member, dapatkan diskon 10% dari program membership.");
+        }
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode promo diterapkan! Anda mendapatkan DISKON 50% 🎉");
+        } else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo diterapkan! Anda mendapatkan DISKON 30% 🎉");
+        } else {
+            System.out.println("Kode promo invalid");
         }
 
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
-        System.out.println("2. Cappuccino - Rp 20,ooo");
+        System.out.println("2. Cappuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
         System.out.println("4. Teh Tarik - Rp 12,000");
         System.out.println("5. Roti Bakar - Rp 10,000");
@@ -20,6 +29,6 @@ public class kafe27 {
     }
 
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON30"); 
     }
 }
