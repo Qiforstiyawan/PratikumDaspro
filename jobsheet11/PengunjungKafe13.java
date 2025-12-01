061cbca99d905475c656bdf119e3e407911e4422
Qiforstiyawan <qiforstiyawan@gmail.com>
2025-12-01 22:00:52 +0700

    Percobaan 4

---
+++ jobsheet11/PengunjungKafe13.java
@@ -0,0 +1,17 @@
+package jobsheet11;
+
+public class PengunjungKafe13 {
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+
+    }
+
+    static void daftarPengunjung(String... namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung:");
+        for (String string : namaPengunjung) {
+            System.out.println("- " + string);
+        }
+    }
+}
